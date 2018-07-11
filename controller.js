a222bc747614ac004a4a637493447a450dbd7cc2
scott88lee <scott88lee@gmail.com>
2018-07-11 14:18:10 +0800

    ...

--- controller.js
+++ controller.js
@@ -1,18 +1,78 @@
 const db = require('./db');
 
 module.exports = {
-	blah : function saySomething() {
-				console.log('Hey there!');
-			},
 
-	listProducts : (req, res) => {
+	getListProducts : (req, res) => {
 		const queryString = "SELECT * FROM products INNER JOIN suppliers ON products.supplier = suppliers.supplier_id";
 
 		db.pool.query(queryString, (err, result) => {
 	    	if (err) {
 	    	console.error('List product Query error:', err.stack);
 	   		}
-	    	res.render('listProducts', {productList : result.rows});
+	    	res.render('products/listAll', {productList : result.rows});
+		});
+	},
+
+	getAddProduct : (req, res) => {
+		const queryString = "SELECT * FROM suppliers";
+		//To retrieve supplier l
+		
+		db.pool.query(queryString, (err, result) => {
+		    if (err) {
+		      console.error('Query suppliers error:', err.stack);
+		    }
+		    res.render('products/add', {supplierList : result.rows});
+		});
+	},
+
+	getBySKU : (req, res) => {
+		const queryString = "SELECT * FROM products INNER JOIN suppliers ON products.supplier = suppliers.supplier_id WHERE SKU = '" + req.params.sku + "'";
+			
+		db.pool.query(queryString, (err, result) => {
+		    if (err) {
+		      console.error('Query showProduct error:', err.stack);
+		    }
+		    console.log(result.rows);
+		    res.render('products/showOne', {product : result.rows[0]});
+		});
+	},
+
+	postAddProduct : (req, res) => {
+		const queryString = "INSERT INTO products (SKU, brand, model, supplier, description, color, variation) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
+		const values = [req.body.SKU, req.body.brand, req.body.model, req.body.supplier, req.body.description, req.body.color, req.body.variation];
+		
+		db.pool.query(queryString, values, (err, result) => {
+		    if (err) {
+		      console.error('Post product Query error:', err.stack);
+		    }
+		    res.send('Added : ' + result.rows[0].model);
+		});
+	},
+
+	getListSuppliers : (req, res) => {
+		const queryString = "SELECT * FROM suppliers";
+
+		db.pool.query(queryString, (err, result) => {
+			if (err) {
+				console.log(err.stack);
+			}
+			res.render('suppliers/listAll', {suppliers : result.rows})
+		});
+	},
+
+	getAddSupplier : (req, res) => {
+		res.render('suppliers/add');
+	},
+
+	postAddSupplier : (req, res) => {
+		const queryString = "INSERT INTO suppliers (supplier_name, business_name, supplier_address) VALUES ($1, $2, $3) RETURNING *";
+		const values = [req.body.supplier_name, req.body.business_name, req.body.supplier_address];
+		
+		db.pool.query(queryString, values, (err, result) => {
+		    if (err) {
+		      console.error('Post supplier Query error:', err.stack);
+		    }
+		    res.send('Added : ' + result.rows[0].supplier_name);
 		});
 	}
 }
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// IMPORTS
 const express = require('express');
 const controller = require('./controller');
 const db = require('./db');
-
 const app = express();
+// IMPORTS
+
 
 // Set React jsx to be the default view engine
 const reactEngine = require('express-react-views').createEngine();
@@ -10,52 +12,22 @@ app.engine('jsx', reactEngine);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.use(express.urlencoded({ extended: true }));
+// Set React jsx to be the default view engine
+
+// ROUTES
+app.get('/products', controller.getListProducts);
+app.get('/products/new', controller.getAddProduct);
+app.get('/products/:sku', controller.getBySKU);
+app.post('/products', controller.postAddProduct);
+
+app.get('/suppliers', controller.getListSuppliers);
+app.get('/suppliers/new', controller.getAddSupplier); //
+//app.get('/supplier/:alias', controller.getSupplierByAlias);
+app.post('/suppliers', controller.postAddSupplier);
 
 
-app.get('/', (req, res) => {
-	res.render('home');
-});
+app.get('/', (req, res) => { res.render('home') });
+app.get('*', (req, res) => { res.sendStatus(404) });
 
-app.get('/products', controller.listProducts);
-
-app.get('/products/new', (req, res) => {
-	const queryString = "SELECT * FROM suppliers";
-	//To retrieve supplier l
-	
-	db.pool.query(queryString, (err, result) => {
-	    if (err) {
-	      console.error('Query suppliers error:', err.stack);
-	    }
-	    res.render('addProduct', {supplierList : result.rows});
-	});
-});
-
-app.get('/products/:sku', (req, res) => {
-	const queryString = "SELECT * FROM products INNER JOIN suppliers ON products.supplier = suppliers.supplier_id WHERE SKU = '" + req.params.sku + "'";
-		
-	db.pool.query(queryString, (err, result) => {
-	    if (err) {
-	      console.error('Query showProduct error:', err.stack);
-	    }
-	    console.log(result.rows);
-	    res.render('showProduct', {product : result.rows[0]});
-	});
-});
-
-app.post('/products', (req, res) => {
-	const queryString = "INSERT INTO products (SKU, brand, model, supplier, description, color, variation) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
-	const values = [req.body.SKU, req.body.brand, req.body.model, req.body.supplier, req.body.description, req.body.color, req.body.variation];
-	
-	db.pool.query(queryString, values, (err, result) => {
-	    if (err) {
-	      console.error('Post product Query error:', err.stack);
-	    }
-	    res.send('Added : ' + result.rows[0].model);
-	});
-});
-
-app.get('*', (req, res) => {
-	res.sendStatus(404);
-})
-
-app.listen(3000, ()=> {console.log('Listen port: 3000')})
+// LISTEN
+app.listen(3000, () => {console.log('Listen port: 3000')} );
